0e74e048b34d8b7700918f2c2c109740903141b4
Krzysztof Maicher <krzysztof.maicher@gmail.com>
2017-01-27 14:15:24 +0100

    Add concurrent offers downloading

--- oms.rb
+++ oms.rb
@@ -85,25 +85,29 @@ scraper.page(url) do |page|
   path = "results/#{search_result_page.title.gsub(' ', '_')}_#{Time.now.to_i}.csv"
   CSV.open(path, 'wb') do |csv|
     csv << %w(Adres Marka Model Napęd Skrzynia Rocznik Silnik Przebieg Kraj Cena Url)
-    search_result_page.offer_urls.each do |offer_url|
-      scraper.page(offer_url) do |offer_page|
-        offer = OfferPage.new(offer_page)
-        csv << [].tap do |arr|
-          arr << offer.address
-          arr << offer.param('Marka')
-          arr << offer.param('Model')
-          arr << offer.param('Napęd')
-          arr << offer.param('Skrzynia biegów')
-          arr << offer.param('Rok produkcji')
-          arr << offer.param('Pojemność skokowa').gsub('cm3', '').to_i
-          arr << offer.param('Przebieg').gsub('km', '').to_i
-          arr << offer.param('Kraj pochodzenia')
-          arr << (offer.brutto? ? offer.price : offer.price * 1.23).to_i
-          arr << offer_url
-          print '.'
+    [].tap do |threads|
+      search_result_page.offer_urls.each do |offer_url|
+        threads << Thread.new do
+          scraper.page(offer_url) do |offer_page|
+            offer = OfferPage.new(offer_page)
+            csv << [].tap do |arr|
+              arr << offer.address
+              arr << offer.param('Marka')
+              arr << offer.param('Model')
+              arr << offer.param('Napęd')
+              arr << offer.param('Skrzynia biegów')
+              arr << offer.param('Rok produkcji')
+              arr << offer.param('Pojemność skokowa')&.gsub('cm3', '')&.to_i
+              arr << offer.param('Przebieg')&.gsub('km', '')&.to_i
+              arr << offer.param('Kraj pochodzenia')
+              arr << (offer.brutto? ? offer.price : offer.price * 1.23).to_i
+              arr << offer_url
+              print '.'
+            end
+          end
         end
       end
-    end
+    end.each(&:join)
     puts "\nDone. Results saved to #{path}"
   end
 end
